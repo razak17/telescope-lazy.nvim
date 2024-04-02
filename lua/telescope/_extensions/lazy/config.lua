addd8b00434e88130a2d98d5a23e0f4117a73403
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2024-04-02 22:34:30 +0300

    feat(actions): Add `auto_close` configuration for `change_cwd_to_plugin`

--- lua/telescope/_extensions/lazy/config.lua
+++ lua/telescope/_extensions/lazy/config.lua
@@ -18,6 +18,14 @@ M.defaults = {
     open_lazy_root_live_grep = "<C-r>g",
     change_cwd_to_plugin = "<C-c>d",
   },
+  actions_opts = {
+    open_in_browser = {
+      auto_close = false,
+    },
+    change_cwd_to_plugin = {
+      auto_close = false,
+    },
+  },
   terminal_opts = {
     relative = "editor",
     style = "minimal",
@@ -26,11 +34,6 @@ M.defaults = {
     title_pos = "center",
     width = 0.5,
     height = 0.5,
-  },
-  actions_opts = {
-    open_in_browser = {
-      auto_close = false,
-    },
   },
 }
 
